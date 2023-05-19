c5e10b52ac87f4ea8a07b65acf479922baeb570c
Sam Komarov <sam@skomarov.com>
2023-05-19 16:02:10 +0300

    solve task #2

---
+++ task_2.c
@@ -0,0 +1,32 @@
+#include <ctype.h>
+#include <stdio.h>
+
+struct Person {
+  int age; 
+  char name[30]; 
+};
+
+float average_age(struct Person arr[], int n) {
+  float sum = 0; 
+  for (int i = 0; i < n; ++i) {
+    sum += arr[i].age; 
+  }
+  return sum/n; 
+}
+
+void scan_array(int n, struct Person a[n]) {
+  for (int i = 0; i < n; ++i) {
+    scanf("%d %s", &a[i].age, a[i].name); 
+  }
+}
+
+int main() {
+  int n; 
+  scanf("%d", &n); 
+  struct Person a[n]; 
+
+  scan_array(n, a); 
+  printf("%f\n", average_age(a, n)); 
+
+  return 0;
+}
