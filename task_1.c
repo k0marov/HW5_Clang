c5c6502d9340297a8e547cd4504118e8bb7b5c92
Sam Komarov <sam@skomarov.com>
2023-05-19 15:58:05 +0300

    solve task #1

---
+++ task_1.c
@@ -0,0 +1,34 @@
+#include <ctype.h>
+#include <stdio.h>
+
+// n needs to be even 
+void swap_halves(int n, int a[n], int res[n]) {
+  int middle = n/2; 
+  for (int i = 0; i < n; ++i) {
+    res[i] = i >= middle ? a[i-middle] : a[middle+i]; 
+  }
+}
+
+void scan_array(int n, int a[n]) {
+  for (int i = 0; i < n; ++i) {
+    scanf("%d", &a[i]); 
+  }
+}
+void print_array(int n, int a[n]) {
+  for (int i = 0; i < n; ++i) {
+    printf("%d ", a[i]); 
+  }
+  printf("\n"); 
+}
+
+int main() {
+  int n; 
+  scanf("%d", &n); 
+  int a[n], b[n]; 
+
+  scan_array(n, a); 
+  swap_halves(n, a, b); 
+  print_array(n, b);
+
+  return 0;
+}
