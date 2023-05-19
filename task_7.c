ab4820530145143f4c9af9eb2d29d76de6b74c9f
Sam Komarov <sam@skomarov.com>
2023-05-19 20:09:05 +0300

    solve task #7

---
+++ task_7.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <stdio.h>
+
+long long count_seq_without_triple_ones(int n) {
+  long long dp[n > 4 ? n : 4]; 
+  dp[0] = 1; dp[1] = 2; dp[2] = 4; dp[3] = 7; 
+  for (int i = 4; i <= n; ++i) {
+    dp[i] = dp[i-1]*2-dp[i-4];
+  }
+  return dp[n]; 
+}
+
+int main() {
+  int n; 
+  scanf("%d", &n); 
+  printf("%lld\n", count_seq_without_triple_ones(n));
+  return 0;
+}
