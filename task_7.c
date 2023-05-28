68ab32333e4de7f21adc98701e0406138eaea7d0
Sam Komarov <sam@skomarov.com>
2023-05-28 16:19:46 +0300

    make array size be n+1 instead of n in task #7

--- task_7.c
+++ task_7.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 long long count_seq_without_triple_ones(int n) {
-  long long dp[n > 4 ? n : 4]; 
+  long long dp[n > 4 ? n+1 : 4]; 
   dp[0] = 1; dp[1] = 2; dp[2] = 4; dp[3] = 7; 
   for (int i = 4; i <= n; ++i) {
     dp[i] = dp[i-1]*2-dp[i-4];
